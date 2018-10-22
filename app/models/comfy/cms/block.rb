3bd086e150815b29e0faccd781b6eb24e55ce031
Marc Sardon <pringaohowto@gmail.com>
2018-10-22 13:00:54 +0100

    Refactor CMS block rubocop issues and group logic
    - There were some rubocop alerts on the file.
    - Refactored some complex methods extracting logic.
    - Simplified from multiple conditional checks to single one directing
      that flow behaviour.
    - Extracted the "mark for destruction" from the temporal files loop as
      doesn't make sense this call being triggered from every single
      temporal file (as it acts over all files collection without any
      dependency on the temp files).
      Yes, when "single_file" is true also means that the temp_files
      collection will contain just a single file, so the loop would only
      iterate once. But this is hard to read/understand and doesn't seem to
      belong inside that loop. That is why I extracted the "set a single
      temp file and mark all the files for destruction" into a separated
      block/method outside this "building files" loop, that should be
      independent for that previous conditional logic.

--- app/models/comfy/cms/block.rb
+++ app/models/comfy/cms/block.rb
@@ -1,27 +1,31 @@
 class Comfy::Cms::Block < ActiveRecord::Base
   self.table_name = 'comfy_cms_blocks'
 
-  FILE_CLASSES = %w(ActionDispatch::Http::UploadedFile Rack::Test::UploadedFile File).freeze
+  FILE_CLASSES = %w[ActionDispatch::Http::UploadedFile
+                    Rack::Test::UploadedFile File].freeze
 
   attr_accessor :temp_files
 
   # -- Relationships --------------------------------------------------------
   belongs_to :blockable,
-    polymorphic: true
+             polymorphic: true
+
   has_many :files,
-    autosave: true,
-    dependent: :destroy
+           autosave: true,
+           dependent: :destroy
 
   # -- Callbacks ------------------------------------------------------------
   before_save :prepare_files
 
   # -- Validations ----------------------------------------------------------
-  validates :identifier, :presence => true
+  validates :identifier, presence: true
 
   # -- Instance Methods -----------------------------------------------------
   # Tag object that is using this block
   def tag
-    @tag ||= blockable.tags(:reload).detect{|t| t.is_cms_block? && t.identifier == identifier}
+    @tag ||= blockable.tags(:reload).detect do |t|
+      t.is_cms_block? && t.identifier == identifier
+    end
   end
 
   # Intercepting assigns as we can't cram files into content directly anymore
@@ -29,31 +33,38 @@ class Comfy::Cms::Block < ActiveRecord::Base
     self.temp_files = [value].flatten.select do |f|
       FILE_CLASSES.member?(f.class.name)
     end
-
     # correctly triggering dirty
-    value = nil               if self.temp_files.present?
-    write_attribute(:content, value)
-    self.content_will_change! if self.temp_files.present?
+    if temp_files.present?
+      write_attribute(:content, nil)
+      content_will_change!
+    else
+      write_attribute(:content, value)
+    end
   end
 
   protected
 
   # If we're passing actual files into content attribute, let's build them.
   def prepare_files
-    return if self.temp_files.blank?
+    return if temp_files.blank?
 
-    # only accepting one file if it's PageFile. PageFiles will take all
-    single_file = self.tag.instance_of?(ComfortableMexicanSofa::Tag::PageFile)
-    self.temp_files = [self.temp_files.first].compact if single_file
+    restrict_to_single_file! if pagefile?
 
-    self.temp_files.each do |file|
-      self.files.collect{ |f| f.mark_for_destruction } if single_file
-
-      self.files.build(
-        site: self.blockable.site,
-        dimensions: self.tag.try(:dimensions),
+    temp_files.each do |file|
+      files.build(
+        site: blockable.site,
+        dimensions: tag.try(:dimensions),
         file: file
       )
     end
+  end
+
+  def pagefile?
+    tag.instance_of?(ComfortableMexicanSofa::Tag::PageFile)
+  end
+
+  def restrict_to_single_file!
+    self.temp_files = [temp_files.first].compact
+    files.each(&:mark_for_destruction)
   end
 end
